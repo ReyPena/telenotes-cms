a19b3417cf441bd6063e411bbc71ff056e264aaa
ReyPena <zonickblack@gmail.com>
2018-01-27 16:46:49 -0700

    Create dashboard module structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
