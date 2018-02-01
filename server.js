429ad7a083a34742f6bf9db1dd330e5a0f42dad7
ReyPena <zonickblack@gmail.com>
2018-02-01 03:32:01 -0700

    Add proxy to the server.

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+app.use('/api', proxy({target: 'http://devapp.telenotes.com', changeOrigin: true, secure: false}));
+
 app.listen(process.env.PORT || 8080);
